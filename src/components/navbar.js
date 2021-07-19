2d148ff53b0aa395a23e05f6ff7b9e43206f4030
Will Cohen <whysgai@gmail.com>
2021-07-19 19:22:03 -0400

    style: Center navbar text

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,8 @@
 const NavBar = props =>
-    <div>
-        <nav class="navbar navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Glossary of Dark Patterns</a>
-            </div>
-        </nav>
-    </div>
+    <nav class="navbar navbar-light bg-light">
+        <div class="navdiv">
+            <a class="navbar-brand" href="#">Glossary of Dark Patterns</a>
+        </div>
+    </nav>
 
 export default NavBar;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import bootstrap from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/index.css';
+import './styles/styles.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
