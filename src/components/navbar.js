bed55b438fea48fa318de0ffbba850107b5745b0
Will Cohen <whysgai@gmail.com>
2021-07-19 19:12:53 -0400

    feat: Add navbar component

---
+++ src/components/navbar.js
@@ -0,0 +1,10 @@
+const NavBar = props =>
+    <div>
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Glossary of Dark Patterns</a>
+            </div>
+        </nav>
+    </div>
+
+export default NavBar;
