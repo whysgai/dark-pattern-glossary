3888e40d40e97ebca92baeffecd9973ace49708c
Will Cohen <will.r.cohen@gmail.com>
2021-07-26 17:49:07 -0400

    feat: Add definition pane to index

--- src/components/GlossaryIndex.js
+++ src/components/GlossaryIndex.js
@@ -1,7 +1,16 @@
+import Indices from "../data/indices.js"
+
 const GlossaryIndex = props =>
     <div className="glossary-index list-group list-group-flush">
-        <div className="list-group-item"><span>Deceptive Timers</span></div>
-        <div className="list-group-item"><span>Switched Answers</span></div>
+        {
+            Indices.indices.map((entry, index) => 
+                <div key={index}
+                className="list-group-item">
+                    <span>{entry}</span>
+                </div>
+            )
+        }
+        <div className="bg-light glossary-definition"><span>Hello</span></div>
     </div>
 
 export default GlossaryIndex;
