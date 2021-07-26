e9e5715bec2c7a5fa0a8b2da1ec20d611d2b5af2
Will Cohen <will.r.cohen@gmail.com>
2021-07-26 17:32:10 -0400

    style: Add header, borders, and spacing

--- src/components/GlossaryIndex.js
+++ src/components/GlossaryIndex.js
@@ -1,6 +1,7 @@
 const GlossaryIndex = props =>
-    <div className="glossary-index">
-        <span>Index</span>
+    <div className="glossary-index list-group list-group-flush">
+        <div className="list-group-item"><span>Deceptive Timers</span></div>
+        <div className="list-group-item"><span>Switched Answers</span></div>
     </div>
 
 export default GlossaryIndex;
--- src/components/glossary.js
+++ src/components/glossary.js
@@ -2,8 +2,9 @@ import GlossaryIndex from '../components/GlossaryIndex.js';
 import GlossaryPage from '../components/GlossaryPage.js';
 
 const Glossary = props =>
-    <div class="card glossary-card">
-        <div class="card-body">
+    <div className="card glossary-card">
+        <div className="card-header bg-light"><h2>Title</h2></div>
+        <div className="glossary-card-body">
             <GlossaryIndex/>
             <GlossaryPage/>
         </div>
