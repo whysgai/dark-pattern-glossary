e6a587748a0e771b18e74cd24fdee619663ae8dd
Will Cohen <will.r.cohen@gmail.com>
2021-07-26 17:49:35 -0400

    feat: Add list of entries

---
+++ src/data/indices.js
@@ -0,0 +1,12 @@
+export default {
+    indices : [
+        "Additional Items",
+        "Confirm-shaming",
+        "Deceptive Timers",
+        "Disguised Ads",
+        "Forced Continuity",
+        "Opt-Ins",
+        "Switchted Answers",
+        "Trammel Net"
+    ]
+}
