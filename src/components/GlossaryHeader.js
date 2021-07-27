5a49711788d2342703600727f7f0aafff43dd4c7
Will Cohen <will.r.cohen@gmail.com>
2021-07-26 20:45:41 -0400

    feat: Implement change title on selection

--- src/components/GlossaryHeader.js
+++ src/components/GlossaryHeader.js
@@ -1,4 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBook } from "@fortawesome/free-solid-svg-icons";
+
 const GlossaryHeader = props =>
-    <div className="card-header bg-light"><h2>Title</h2></div>
+    <div className="card-header bg-light">
+        {/* <FontAwesomeIcon icon={faBook} /> */}
+        <h2>{props.activeIndex}</h2>
+    </div>
 
 export default GlossaryHeader;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import bootstrap from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/index.css';
