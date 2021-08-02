e4012084eff2a0b61c6e719c1e63db4dd2bde641
Will Cohen <will.r.cohen@gmail.com>
2021-08-02 17:33:58 -0400

    feat: Add disguised ads entries

--- src/components/DisguisedAds.js
+++ src/components/DisguisedAds.js
@@ -1,9 +1,98 @@
 const DisguisedAds = () => 
     <div>
         <h3>Search Results</h3>
-        <div>
+        <label>
+            <input placeholder="pie"/>
+        </label>
+        <ul className="results-list">
+            <li className="result">
+                <span className="result-prefix">Star</span>
+                <span className="result-text">Pi in the Sky Math Tutoring</span>
+                <span>3.75/5</span>
+            </li>
+            <li className="result">
+                <span className="result-prefix">1</span>
+                <span className="result-text">Blackbird Pie Company</span>
+                <span>4.5/5</span>
+            </li>
+            <li className="result">
+                <span className="result-prefix">Star</span>
+                <span className="result-text">Big Apple Pizza by the Slice</span>
+                <span>3.25/5</span>
+            </li>
+            <li className="result">
+                <span className="result-prefix">2</span>
+                <span className="result-text">Gran-Gran's Pies</span>
+                <span>5/5</span>
+            </li>
+            <li className="result">
+                <span className="result-prefix">3</span>
+                <span className="result-text">Pie Are Round Bakery</span>
+                <span>2/5</span>
+            </li>
+            <li className="result">
+                <span className="result-prefix">4</span>
+                <span className="result-text">Queen of Hearts Tarts and More</span>
+                <span>4.25/5</span>
+            </li>
+            <li className="result">
+                <span className="result-prefix">5</span>
+                <span className="result-text">Ye Old Pie Shoppe</span>
+                <span>5/5</span>
+            </li>
+            <li className="result">
+                <span className="result-prefix">6</span>
+                <span className="result-text">O'Hare Deep Dish Pizza Pies</span>
+                <span>3/5</span>
+            </li>
+            <li className="result">
+                <span className="result-prefix">7</span>
+                <span className="result-text">Best Bakery Cakes and More</span>
+                <span>5/5</span>
+            </li>
+            <li className="result">
+                <span className="result-prefix">8</span>
+                <span className="result-text">Townsville Bakery</span>
+                <span>3/5</span>
+            </li>
+            <li className="result">
+                <span className="result-prefix">9</span>
+                <span className="result-text">The King's Crown: Pub, Ales, and Meatpies</span>
+                <span>3.5/5</span>
+            </li>
+            <li className="result">
+                <span className="result-prefix">10</span>
+                <span className="result-text">Patti-Cakes Handpies</span>
+                <span>5/5</span>
+            </li>
+            <li className="result">
+                <span className="result-prefix">11</span>
+                <span className="result-text">Easy As... Take and Bake Pies</span>
+                <span>4.25/5</span>
+            </li>
+            <li className="result">
+                <span className="result-prefix">12</span>
+                <span className="result-text">Piece of... Take and Bake Cakes</span>
+                <span>4.25/5</span>
+            </li>
+            <li className="result">
+                <span className="result-prefix">13</span>
+                <span className="result-text">Main Street Bakery</span>
+                <span>3.5/5</span>
+            </li>
+            <li className="result">
+                <span className="result-prefix">14</span>
+                <span className="result-text">Cameron's Confections</span>
+                <span>2.5/5</span>
+            </li>
+            <li className="result">
+                <span className="result-prefix">15</span>
+                <span className="result-text">Dessert Island</span>
+                <span>5/5</span>
+            </li>
+            
 
-        </div>
+        </ul>
     </div>
 
 export default DisguisedAds;
