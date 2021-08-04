3bfc5270f989ded56ac8a9823e197079657f101b
Will Cohen <will.r.cohen@gmail.com>
2021-08-04 18:53:45 -0400

    feat: Add validation and styling to forced continuity

--- src/components/ForcedContinuity.js
+++ src/components/ForcedContinuity.js
@@ -2,65 +2,83 @@ import { useState } from "react";
 
 const ForcedContinuity = () => {
     const [page, setPage] = useState(0);
+    const [fname, setFname] = useState("");
+    const [lname, setLname] = useState("");
+    const [email, setEmail] = useState("");
+    const [checked, setChecked] = useState(false);
+    const [cardname, setCardName] = useState("");
+    const [cardnum, setCardNum] = useState("");
+    const [cardCCV, setCardCCV] = useState("");
+
+    const clearAll = () => {
+        setFname("");
+        setLname("");
+        setEmail("");
+        setChecked("");
+    }
 
     return (
-        <div>
-            <h3>Begin Your Free Trial</h3>
-            
-            {
-                page === 0 ?
-                    <>
-                        <h4>Enter Your Contact Info</h4>
-                        <label>
-                            First Name 
-                            <input type="text"></input>
-                        </label>
-                        <label>
-                            Last Name 
-                            <input type="text"></input>
-                        </label>
-                        <label>
-                            Email 
-                            <input type="email"></input>
-                        </label>
-                        <label className="btn btn-outline-primary">
-                            I have read the Terms and Conditions 
-                            <input type="checkbox"></input>
-                        </label>
-                        <button onClick={() => setPage(1)}>Continue</button>
-                    </>
-                    :
-                    page === 1 ?
+        <div className="forced-continuity standard-layout">
+            <div className="card card-body">
+                <h3>Begin Your Free Trial</h3>            
+                {
+                    page === 0 ?
                         <>
-                            <h4>Enter Payment Info</h4>
-                            <label>
-                                Name on Card
-                                <input type="text"></input>
+                            <h5>Enter your contact info:</h5>
+                            <div className="form-floating">
+                                <input type="text" className="form-control" id="forced-con-fname" value={fname}
+                                    onChange={(e) => setFname(e.target.value)}
+                                />
+                                <label htmlFor="forced-con-fname">First Name <span className="req-asterisk">*</span></label>
+                            </div>                            
+                            <div className="form-floating">
+                                <input type="text" className="form-control" id="forced-con-lname" value={lname} onChange={(e) => setLname(e.target.value)}/>
+                                <label htmlFor="forced-con-lname">Last Name <span className="req-asterisk">*</span></label>
+                            </div>
+                            <div className="form-floating">
+                                <input type="text" className="form-control" id="forced-con-email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                                <label htmlFor="forced-con-email">Email Address <span className="req-asterisk">*</span></label>
+                            </div>
+                            <label className="btn btn-outline-success btn-terms" >
+                                I have read the Terms and Conditions <span className="req-asterisk">*</span>
+                                <input type="checkbox" onChange={() => setChecked(!checked)}/>
                             </label>
-                            <label>
-                                Card Number
-                                <input type="text"></input>
-                            </label>
-                            <label>
-                                CCV / Security Code
-                                <input type="email"></input>
-                            </label>
-                            <label>
-                                Expiration Month
-                                <input type="email"></input>
-                            </label>
-                            <label>
-                                Expiration Year
-                                <input type="email"></input>
-                            </label>
-                            <button onClick={() => setPage(2)}>Continue</button>
+                            <button className="btn btn-primary btn-medium-text" onClick={() => {clearAll(); setPage(1)}} 
+                                disabled={fname !== "" && lname !== "" && email !== "" && checked ? false : true}
+                            >
+                                Continue
+                            </button>
+                            <span className="req-legend"><span className="req-asterisk">*</span> = Required</span>
                         </>
                         :
-                        <>
-                            <h3>Thanks for signing up!</h3>
-                        </>
-            }
-            
+                        page === 1 ?
+                            <>
+                                <h5>Enter your payment info:</h5>
+                                <div className="form-floating">
+                                    <input type="text" className="form-control" id="forced-con-cardname" value={cardname} onChange={(e) => setCardName(e.target.value)}/>
+                                    <label htmlFor="forced-con-cardname">Name on Card</label>
+                                </div>
+                                <div className="form-floating">
+                                    <input type="text" className="form-control" id="forced-con-cardnum" value={cardnum} onChange={(e) => setCardNum(e.target.value)}/>
+                                    <label htmlFor="forced-con-cardnum">Card Number</label>
+                                </div>
+                                <div className="form-floating">
+                                    <input type="text" className="form-control" id="forced-con-ccv" value={cardCCV} onChange={(e) => setCardCCV(e.target.value)}/>
+                                    <label htmlFor="forced-con-ccv">CCV / Security Code</label>
+                                </div>
+                                <div className="form-floating">
+                                    <input type="text" className="form-control" id="forced-con-expdate"/>
+                                    <label htmlFor="forced-con-expdate">Experation Date</label>
+                                </div> 
+                                <button onClick={() => setPage(2)}>Continue</button>
+                                <span className="req-legend"><span className="req-asterisk">*</span> = Required</span>
+                            </>
+                            :
+                            <>
+                                <h3>Thanks for signing up!</h3>
+                            </>
+                }
+            </div>            
         </div>
     )
 }
