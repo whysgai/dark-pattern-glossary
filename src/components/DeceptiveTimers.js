87b3b7f64969c2610f1024396852f638ddddf62f
Will Cohen <will.r.cohen@gmail.com>
2021-08-05 17:35:35 -0400

    style: Add styling to deceptive timers

--- src/components/DeceptiveTimers.js
+++ src/components/DeceptiveTimers.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import '../media/download.jfif'
+// import '../media/download.jfif'
 
 const DeceptiveTimers = () => {
     const [page, setPage] = useState(0);
@@ -9,21 +9,26 @@ const DeceptiveTimers = () => {
     return (
         <div className="deceptive-timers standard-layout">
             <div className="card card-body">
-                <div className="store-col">
-                    <img src={"../media/doohicky.jpg"} alt="Doohicky and springs"/>
-                    <p>Description of the doohicky</p>
-                </div>
-                <div className="store-col">
-                    <button className="btn btn-secondary">Cart: <span>0</span></button>
-                    <h3>Doohicky Set</h3>
-                    <h4>Sale price: $19.75 <span className="strikethrough">$35.00</span></h4>
-                    <h4>Sale ends in <span></span></h4>
-                    <h4>Only <span></span> remain at this price</h4>
-                    <label className="form-label">
-                        Quantity:
-                        <input className="form-control" min="1" max="10" placeholder="1" type="number"/>
-                    </label>
-                    <button className="btn btn-outline-success">Add to cart</button>
+                <div className="store-row">                    
+                    <div className="store-col">
+                        <img src="media/doohicky.jpg" alt="Doohicky and springs"/>
+                        <p>Description of the doohicky</p>
+                    </div>
+                    <div className="store-col">
+                        <button className="btn btn-secondary cart-btn">Cart: <span>0</span></button>
+                        <h3>Doohicky Set</h3>
+                        <h4>Sale price: $19.75 <span className="strikethrough">$35.00</span></h4>
+                        <span>Sale ends in <span></span></span>
+                        <span>Only <span></span> remain at this price</span>
+                        <div className="input-group">
+                            <label className="input-group-text">
+                                Quantity:                                
+                            </label>
+                            <input className="form-control" min="1" max="10" placeholder="1" type="number"/>
+                        </div>
+                        
+                        <button className="btn btn-outline-success">Add to cart</button>
+                    </div>
                 </div>
             </div>          
         </div>
