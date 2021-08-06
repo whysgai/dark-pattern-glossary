be75de0de91a16326ff6cba783e311fd164cb320
Will Cohen <will.r.cohen@gmail.com>
2021-08-06 18:11:48 -0400

    style: Update header styling

--- src/components/GlossaryHeader.js
+++ src/components/GlossaryHeader.js
@@ -3,7 +3,8 @@ import { faBook } from "@fortawesome/free-solid-svg-icons";
 
 const GlossaryHeader = props =>
     <div className="glossary-header card-header bg-light">
-        <span><span className="h2"><FontAwesomeIcon icon={faBook}/></span><span className="h2"> Glossary of Dark Patterns</span></span>
+        <span className="h2"><FontAwesomeIcon icon={faBook}/></span>
+        <span><span className="h2"> Glossary of Dark Patterns</span></span>
         <span><span className="h2">{props.activeIndex}</span></span>
         
         {/* <h2>{props.activeIndex}</h2> */}
