86b190962debf5b009e6b746fa42e07aff7c405b
Will Cohen <will.r.cohen@gmail.com>
2021-08-09 20:36:42 -0400

    chore: Add fonts

--- src/components/GlossaryHeader.js
+++ src/components/GlossaryHeader.js
@@ -4,8 +4,8 @@ import { faBook } from "@fortawesome/free-solid-svg-icons";
 const GlossaryHeader = props =>
     <div className="glossary-header card-header bg-light">
         <span className="h2"><FontAwesomeIcon icon={faBook}/></span>
-        <span><span className="h2"> Glossary of Dark Patterns</span></span>
-        <span><span className="h2">{props.activeIndex}</span></span>
+        <span><span className="h2 title-font"> Glossary of Dark Patterns</span></span>
+        <span><span className="h2 title-font">{props.activeIndex}</span></span>
     </div>
 
 export default GlossaryHeader;
